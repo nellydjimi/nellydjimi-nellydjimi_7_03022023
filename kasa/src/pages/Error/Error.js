2cb780a67d08aa7a066cb336dbc2e207ef50135f
nellydjimi <djiminelly@gmail.com>
2023-02-20 21:58:04 +0100

    maj

--- kasa/src/pages/Error/Error.js
+++ kasa/src/pages/Error/Error.js
@@ -1,12 +1,19 @@
 import '../Error/error.scss'
+import Header from '../../composants/header/Header'
+import Footer from '../../composants/footer/Footer';
 import { Link } from 'react-router-dom';
 
 const Error = () => {
+
     return (
+        <div>
+        <Header />
         <div className='error'>
-        <h1>404</h1>
-        <p>OUps ! La page que vous demandez n'existe pas. </p>
-            <Link>Retourner sur la page d'accueil</Link>
+        <h1 className='error_title'>404</h1>
+        <p className='error_content'>Oups ! La page que vous demandez n'existe pas. </p>
+            <Link to='/' className='error_home'>Retourner sur la page d'accueil</Link>
+        </div>
+        <Footer />
         </div>
     )
 }
---
+++ kasa/src/pages/Logements/Logements.js
@@ -0,0 +1,3 @@
+import './logements.scss'
+import Header from '../../composants/header/Header'
+import Footer from '../../composants/footer/Footer'
