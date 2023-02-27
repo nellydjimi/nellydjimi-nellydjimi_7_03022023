10d2cdc2fa9564dc22ff8adad686999582a881c0
nellydjimi <djiminelly@gmail.com>
2023-02-27 17:17:58 +0100

    maj

--- kasa/src/composants/caroussel/Caroussel.jsx
+++ kasa/src/composants/caroussel/Caroussel.jsx
@@ -8,35 +8,30 @@ export default function Caroussel({ picturesSlider }) {
     const [slideshow, updateSlideshow] = useState(0);
 
     const nextSlide = () => {
-        updateSlideshow(slideshow + 1)
-        if (slideshow === picturesSlider.lenght - 1)
+        if (slideshow === picturesSlider.lenght - 1) {
             updateSlideshow(0)
+        } else {
+            updateSlideshow(slideshow + 1)
+        }
     }
 
     const previousSlide = () => {
-        updateSlideshow(slideshow - 1)
-        if (slideshow === 0)
+        if (slideshow === 0) {
             updateSlideshow(picturesSlider.lenght - 1)
+        }
+        else {
+            updateSlideshow(slideshow - 1)
+        }
     }
 
     return (
-        <section style={{backgroundImage : `url(${picturesSlider[slideshow]})`}} className='caroussel'>
-        {picturesSlider.length > 1 && 
-            <>
-                <img 
-                    className='caroussel_arrow caroussel_arrow_right' 
-                    src={ArrowRight} 
-                    alt="show next slider" 
-                    onClick={nextSlide}
-                />
-                <img 
-                    className='caroussel_arrow caroussel_arrow_left' 
-                    src={ArrowLeft} 
-                    alt="show previous slider" 
-                    onClick={previousSlide}
-                />
-            </>
-        } 
-    </section>
+        <section style={{ backgroundImage: `url(${picturesSlider[slideshow]})` }} className='caroussel'>
+            {picturesSlider.length > 1 &&
+                <>
+                    <img className='caroussel_arrow caroussel_arrow_right' src={ArrowRight} alt="show next slider" onClick={nextSlide} />
+                    <img className='caroussel_arrow caroussel_arrow_left' src={ArrowLeft} alt="show previous slider" onClick={previousSlide} />
+                </>
+            }
+        </section>
     )
 }
